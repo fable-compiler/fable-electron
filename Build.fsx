65ceb3756879836b0c54d3ccd0881140a9e3094d
Christer van der Meeren <cmeeren@protonmail.com>
2019-06-19 22:03:55 +0200

    Update for Electron 5 and add infrastructure

--- Build.fsx
+++
@@ -1,9 +0,0 @@
-#r "node-modules/fable-metadata/lib/Fable.Core.dll"
-#load "node_modules/fable-publish-utils/PublishUtils.fs"
-
-open PublishUtils
-
-match args with
-| IgnoreCase "publish"::_ ->
-    pushNuget "src/Fable.Electron.fsproj"
-| _ -> ()
--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+### 1.0.0-beta.01
+
+* Updated for Electron 5.0
+
 ### 1.0.0-alpha-001
 
 * First release
